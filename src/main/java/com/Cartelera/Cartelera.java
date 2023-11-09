580f13905e482d45c2ec7d0072edb3532a8adca4
Ricardo2011-Quesada <joquesada2009@hotmail.com>
2023-11-09 17:22:10 -0600

    Initial commit

---
+++ src/main/java/com/Cartelera/Cartelera.java
@@ -0,0 +1,13 @@
+package com.Cartelera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cartelera {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cartelera.class, args);
+	}
+
+}
---
+++ src/main/java/com/Cartelera/Controller/CarteleraController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/SpringFramework/Controller.java to edit this template
+ */
+package com.Cartelera.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@Slf4j
+public class CarteleraController {
+    
+    @RequestMapping("/")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "index";
+    }
+    @GetMapping("/InicioSesion")
+    public String InicioSesion(Model model) {
+        model.addAttribute("attribute", "value");
+        return "InicioSesion";
+    }
+     @GetMapping("/Registro")
+    public String Registro(Model model) {
+        model.addAttribute("attribute", "value");
+        return "Registro";
+    }
+     @GetMapping("/cartelera")
+    public String cartelera(Model model) {
+        model.addAttribute("attribute", "value");
+        return "cartelera";
+    }
+     @GetMapping("/Promociones")
+    public String Promociones(Model model) {
+        model.addAttribute("attribute", "value");
+        return "Promociones";
+    }
+         @GetMapping("/Dulceria")
+    public String Dulceria(Model model) {
+        model.addAttribute("attribute", "value");
+        return "Dulceria";
+    }
+         @GetMapping("/CinePlus")
+    public String CinePlus(Model model) {
+        model.addAttribute("attribute", "value");
+        return "CinePlus";
+    }
+}
